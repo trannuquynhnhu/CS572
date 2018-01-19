59100336ab144919dba2b1fa30de7df4c52ef544
Nu Quynh Nhu <986121@cs.mum.edu>
2018-01-19 15:38:34 -0600

    lab 5

---
+++ Day5/expressApp.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var consolidate = require("consolidate");
+var app = express();
+var port = 1234;
+const fetch = require("node-fetch");
+
+app.engine("ejs", consolidate.ejs);
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.enable("trust proxy");
+
+app.get("*", function(request, response){
+                var promise = fetch("http://jsonplaceholder.typicode.com/users/");
+                    promise.then(data => data.json())
+                            .then(users => {                                
+                                response.render("userlist", {users: users});                                
+                            })
+                            .catch((error) => {console.error(error)});                
+            });
+
+app.listen(port, function(){
+    console.log("The server is running on the port %s", port);
+});
+
