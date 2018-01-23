82ce62e77944bbf6a492646730ffecfe05e5dafc
DESKTOP-UQMD7D2\Quynh Nhu <trannuquynhnhu@gmail.com>
2018-01-22 22:55:38 -0600

    updated

--- Day7/app.js
+++ Day7/app.js
@@ -1,19 +1,27 @@
-var crypto = require('crypto');    
+var express = require("express");
+var crypto = require('crypto');  
+var app = express();  
 var MongoClient = require("mongodb").MongoClient;
 
-MongoClient.connect("mongodb://localhost:27017/lab7", function(error, client){
-    const db = client.db('lab7');
-    if(error) throw error;
-    db.collection("homework7").findOne({}, function(err, doc){
-        if(err) throw err;
-        console.dir(doc.message);
-        var decipher = crypto.createDecipher('aes-256-cbc','asaadsaad')
-        var dec = decipher.update(doc.message,'hex','utf8')
-        dec += decipher.final('utf8');
-        console.dir(dec);
-        client.close();
-
+app.get("*", function(request, response){
+    MongoClient.connect("mongodb://localhost:27017/lab7", function(error, client){
+            const db = client.db('lab7');
+            if(error) throw error;
+            db.collection("homework7").findOne({}, function(err, doc){
+                if(err) throw err;
+                console.dir(doc.message);
+                var decipher = crypto.createDecipher('aes-256-cbc','asaadsaad')
+                var dec = decipher.update(doc.message,'hex','utf8')
+                dec += decipher.final('utf8');
+                response.send(dec);
+                client.close();
+            });
+            console.dir("Finish!");
     });
-    console.dir("Finish!");
 });
+
+app.listen(1234, function(){
+    console.log("The server is listening at the port 1234");
+});
+
 
